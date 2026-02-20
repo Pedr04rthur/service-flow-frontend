04dbf0c8f281bddb96285764760f2ba5535a1f3d
Pedro Arthur <pedro.arthurnery11@gmail.com>
2026-02-20 01:23:33 -0300

    praticando javascript no jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
 
+
+  const [contador, setContador, tiraContador] = useState(0)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Service Flow</h1>
+      <p>Contador: {contador}</p>
+      <button onClick={()=> setContador(contador + 1)}>Aumentar</button>
+      <button onClick={() => setContador (contador - 1)}>Diminuir</button>
+      <button onClick={() => setContador(0)}>Resetar</button>
+    </div>
+    
   )
 }
+
 
 export default App
